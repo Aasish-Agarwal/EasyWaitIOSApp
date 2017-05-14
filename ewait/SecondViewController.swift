42cf5546a4536d13e8d049e910351cf5a5ba252c
Aakansha <aakansha@Aakanshas-MacBook-Pro.local>
2017-05-14 20:06:36 +0530

    Initial Commit

---
+++ ewait/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ewait
+//
+//  Created by Aakansha on 14/05/17.
+//  Copyright © 2017 Smart Creatives. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
